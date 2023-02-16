babfb9b93c2f2ef77009b00bfbf8f8341ac59cff
Juliana Nielson <nielsonjuliana@gmail.com>
2023-02-16 08:07:34 -0700

    Initial Commit

---
+++ Tally App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally App
+//
+//  Created by Juliana Nielson on 2/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
